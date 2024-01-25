a7ea3703344e926ba277185417d6e25c4370cab2
Diogo Gomes <diogogomescastro@outlook.com>
2024-01-25 20:19:53 -0300

    Bugs fix

--- trabalho/src/App.test.js
+++ trabalho/src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders App', () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const linkElement = screen.getByText('Há uma semente de planta nas costas desde o dia em que este Pokémon nasce. A semente cresce de acordo com a melhoria de UX.');
   expect(linkElement).toBeInTheDocument();
 });
